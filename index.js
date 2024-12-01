09a7deec85ec206e9f86dd31f2ac7ceb4b315d7f
ataur rahman <ataurrahmanbro@gmail.com>
2024-12-01 16:50:33 +0600

    data deleted oparation added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4jm04.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -25,18 +25,35 @@ async function run() {
 
     const notpadCollection = client.db('notpad').collection('single-notpad');
 
+    // to save data in local store and mongo database
     app.get('/add-node', async (req, res) => {
         const cursor = notpadCollection.find();
         const result = await cursor.toArray();
         res.send(result)
     })
 
+    // to post data in mongo DB and locally
     app.post('/add-node', async (req, res) => {
         const note = req.body;
         const result = await notpadCollection.insertOne(note);
         res.send(result)
     })
 
+    // to get data for delete and get spacific id
+    app.get('/add-node/:id', async(req, res) => {
+        const id = req.params.id;
+        const quary = {_id: new ObjectId(id)};
+        const result = await notpadCollection.findOne(quary);
+        res.send(result) 
+    })
+
+    // to delete item as per the id
+    app.delete('/add-node/:id', async(req, res) => {
+        const id = req.params.id;
+        const quary = {_id: new ObjectId(id)}
+        const result = await notpadCollection.deleteOne(quary);
+        res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
